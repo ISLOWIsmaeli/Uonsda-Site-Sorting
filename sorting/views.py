07d0607b1b7c774a08ae71452e753db8b50609a3
Ismael126895 <Lenojore67@gmail.com>
2024-02-02 16:30:29 +0300

    created first views

--- sorting/views.py
+++ sorting/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest
 # Create your views here.
+def startpage(request:HttpRequest,*args,**kwargs):
+    return render(request,"sorting/home.html",{})
