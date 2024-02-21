a317f93c8ba806e87687164c6161ec5fbfcb0a7c
Ismael126895 <Lenojore67@gmail.com>
2024-02-21 14:22:25 +0300

    added contacs and about page links

--- sorting/urls.py
+++ sorting/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.startpage, name="start_page"),
+    path("",views.startPage, name="start_page"),
     path("/contacts",views.contactPage,name="contacts_page"),
-    path("/about",views.aboutPage,name="about"),
+    path("/about",views.aboutPage,name="about_page"),
 ]
