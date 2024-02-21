d44eb258c2d7f1352184744df552f1c95b7224dc
Ismael126895 <Lenojore67@gmail.com>
2024-02-21 17:24:39 +0300

    sign up and login working

--- sorting/urls.py
+++ sorting/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("",views.startPage, name="start_page"),
-    path("/contacts",views.contactPage,name="contacts_page"),
-    path("/about",views.aboutPage,name="about_page"),
-    path("/login",views.loginView,name="login_page"),
-    path("/signUp",views.signUpView,name="sign_up_page"),
+    path("contacts",views.contactPage,name="contacts_page"),
+    path("about",views.aboutPage,name="about_page"),
+    path("login",views.loginView,name="login_page"),
+    path("signUp",views.signUpView,name="sign_up_page"),
 ]
