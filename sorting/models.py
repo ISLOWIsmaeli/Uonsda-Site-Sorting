24aaab951e954034753e1b964c45ac9d4285294e
ISLOWIsmaeli <Lenojore67@gmail.com>
2024-07-30 21:51:48 +0300

    added and migrated models

--- sorting/models.py
+++ sorting/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class PersonalInfo(models.Model):
+    name = models.CharField(max_length=244)
+    gender = models.CharField(max_length=244)
+    year_group = models.CharField(max_length=244)
+    campus_church = models.CharField(max_length=244)
