bdae735fec65a124f3ee159088262f471cfcee71
ISLOWIsmaeli <Lenojore67@gmail.com>
2024-08-13 22:19:12 +0300

    models

--- sorting/models.py
+++ sorting/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 class PersonalInfo(models.Model):
+    GENDER_CHOICES = (
+        ('M','Male'),
+        ('F', 'Female')
+    )
     name = models.CharField(max_length=244)
-    gender = models.CharField(max_length=244)
-    year_group = models.CharField(max_length=244)
-    campus_church = models.CharField(max_length=244)
+    gender = models.CharField(max_length=1,choices=GENDER_CHOICES)
+    year_group = models.CharField(max_length=244, null=True, blank=True)
+    campus_church = models.CharField(max_length=244, null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
